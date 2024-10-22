a9f59c07a1c59a28904cd2ec458cc414df1f10b3
Christian Scholz <c.scholz@c-s-media.net>
2024-10-22 15:44:07 +0200

    Added Youtube API to enrich documents with video information

--- src/lolstreamsearch/api/yt_es_documents.py
+++ src/lolstreamsearch/api/yt_es_documents.py
@@ -4,6 +4,8 @@ Youtube Elasticsearch Documents Model
 from elasticsearch_dsl import Text, Date, Keyword, Document
 from rest_framework import serializers
 
+from google_api import get_yt_video_information
+
 
 class YtVideoDocument(Document):
     id = Keyword()
@@ -55,10 +57,10 @@ class YtVideoDocument(Document):
 
 class YtVideoDocumentSerializer(serializers.Serializer):
     id = serializers.CharField(required=False)
-    title = serializers.CharField()
-    description = serializers.CharField()
+    title = serializers.CharField(required=False)
+    description = serializers.CharField(required=False)
     video_url = serializers.CharField()
-    published_at = serializers.DateTimeField()
+    published_at = serializers.DateTimeField(required=False)
     champion = serializers.CharField()
     opponent_champion = serializers.CharField()
     team_champions = serializers.ListField(child=serializers.CharField())
@@ -67,16 +69,31 @@ class YtVideoDocumentSerializer(serializers.Serializer):
     runes = serializers.ListSerializer(child=serializers.CharField())
     champion_items = serializers.ListSerializer(child=serializers.CharField())
     lol_version = serializers.CharField()
-    streamer = serializers.CharField()
+    streamer = serializers.CharField(required=False)
+    views = serializers.IntegerField(required=False)
+    likes = serializers.IntegerField(required=False)
+
+    def get_yt_id(self, instance):
+        return instance["video_url"].split("v=")[1]
 
     def to_representation(self, instance):
         representation = super().to_representation(instance)
         if isinstance(instance, YtVideoDocument):
             representation['id'] = instance.meta.id
+
+        yt_info = get_yt_video_information(self.get_yt_id(instance))
+        representation['views'] = yt_info.views
+        representation['likes'] = yt_info.likes
         return representation
 
     def create(self, validated_data):
-        del validated_data['id']
+        if 'id' in validated_data:
+            del validated_data['id']
+        yt_video_info = get_yt_video_information(self.get_yt_id(validated_data))
+        validated_data['title'] = yt_video_info.title
+        validated_data['description'] = yt_video_info.description
+        validated_data['published_at'] = yt_video_info.published_at
+        validated_data['streamer'] = yt_video_info.channel
         ytvideo = YtVideoDocument(**validated_data)
         result = ytvideo.save(return_doc_meta=True)
         meta_id = result.body.get("_id")
---
+++ src/lolstreamsearch/google_api.py
@@ -0,0 +1,95 @@
+import os
+
+from googleapiclient.discovery import build
+
+api_key = os.getenv("YT_API_KEY")
+
+YOUTUBE = None
+
+
+def get_yt_connection():
+    global YOUTUBE
+    if api_key is None:
+        raise ValueError("YouTube API key is missing")
+    if YOUTUBE is None:
+        YOUTUBE = build('youtube', 'v3', developerKey=api_key)
+    return YOUTUBE
+
+
+"""
+get the youtube video information for the given ID
+
+json response example:
+    
+{'kind': 'youtube#videoListResponse', 
+ 'etag': 'JQMaWSbT0PhxTtk1k-Gts_mpwG0', 
+ 'items':     
+    [{'kind': 'youtube#video', 'etag': 'b0i_5Mbhi4h-HGMoTjFyy1DkdO8', 'id': 'PDuIp8Y9aIY',
+      'snippet': {'publishedAt': '2024-10-21T16:00:34Z', 
+                  'channelId': 'UCcDZST9Rzue7gMruGN8CeIw',
+                  'title': 'VOLIBEAR TOP IS THE #1 NEW 1V5 END BOSS IN SPLIT 3 (QUADRA KILL) - S14 Volibear TOP Gameplay Guide',
+                  'description': "VOLIBEAR TOP IS THE #1 NEW 1V5 END BOSS IN SPLIT 3 (QUADRA KILL) - S14 Volibear TOP Gameplay Guide\nLeague of Legends Season 14 Volibear Toplane Gameplay Guide!\n#volibear #volibearguide #volibeargameplay \n\n🥇Toplane Tierlists & Coaching VODS\nhttps://www.patreon.com/Daveyx3\n\n🎮 TWITCH: https://www.twitch.tv/daveyx3\n🌍 DISCORD: https://discord.gg/t5WKfF7\n📷 INSTAGRAM: https://www.instagram.com/daveyx3/\n👔 MERCH STORE: https://daveyx3-shop.fourthwall.com/\n👉 2ND CHANNEL: https://bit.ly/daveyx3_offmeta\n\n💻 MY PC GEAR: https://www.amazon.com/shop/daveyx3 (Affiliate)\nIf you purchase anything from this link, I'll receive a small commission!\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\nSeason 14 Volibear Runes: \nExplained at beginning of the video.\n\nSeason 14 Volibear Build:\n(nashor tooth - mejai - rabadon - riftmaker - zhonya)\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n[Concepts of this video + Information + Everything you can find on my Channel]:\nLeague of Legends Gameplay Guides, League Gameplay Guides, LoL Gameplay Guides, League of Legends Guide, League Guide, LoL Guide, Season 14 Guide League of Legends, S14 Guide League of Legends, League of Legends Best Runes Season 14, League of Legends Best Build season 14, League of Legends Best Playstyle Season 14, Season 14 Tips, S14 Tips, League of Legends Tips, League of Legends, Daveyx3 Gameplay Channel.\nI'm a Challenger Top Main. Been challenger since S7/S8/S9/S10 & Season 11 and peaked 1109 LP at most. and I upload new video's everyday!",
+                  'thumbnails': {
+                      'default': {'url': 'https://i.ytimg.com/vi/PDuIp8Y9aIY/default.jpg', 'width': 120,
+                                  'height': 90},
+                      'medium': {'url': 'https://i.ytimg.com/vi/PDuIp8Y9aIY/mqdefault.jpg',
+                                 'width': 320, 'height': 180},
+                      'high': {'url': 'https://i.ytimg.com/vi/PDuIp8Y9aIY/hqdefault.jpg', 'width': 480,
+                               'height': 360},
+                      'standard': {'url': 'https://i.ytimg.com/vi/PDuIp8Y9aIY/sddefault.jpg',
+                                   'width': 640, 'height': 480},
+                      'maxres': {'url': 'https://i.ytimg.com/vi/PDuIp8Y9aIY/maxresdefault.jpg',
+                                 'width': 1280, 'height': 720}}, 
+                  'channelTitle': 'Daveyx3 Gameplay',
+                  'tags': ['volibear', 'volibear lol', 'lol volibear', 'volibear league',
+                           'volibear league of legends', 'volibear guide', 'volibear guide s14',
+                           'volibear guide season 14', 'volibear top', 'volibear top guide',
+                           'volibear top guide season 14', 'volibear gameplay', 'volibear gameplay s14',
+                           'volibear gameplay season 14', 'how to play volibear', 'volibear combo',
+                           'volibear runes', 'volibear build', 'volibear build s14',
+                           'volibear build season 14', 'volibear top guide s14', 'volibear s14',
+                           'daveyx3 gameplay', 'daveyx3', 'league of legends'], 
+                  'categoryId': '20',
+                  'liveBroadcastContent': 'none', 
+                  'defaultLanguage': 'en', 
+                  'localized': {
+                    'title': 'VOLIBEAR TOP IS THE #1 NEW 1V5 END BOSS IN SPLIT 3 (QUADRA KILL) - S14 Volibear TOP Gameplay Guide',
+                    'description': "VOLIBEAR TOP IS THE #1 NEW 1V5 END BOSS IN SPLIT 3 (QUADRA KILL) - S14 Volibear TOP Gameplay Guide\nLeague of Legends Season 14 Volibear Toplane Gameplay Guide!\n#volibear #volibearguide #volibeargameplay \n\n🥇Toplane Tierlists & Coaching VODS\nhttps://www.patreon.com/Daveyx3\n\n🎮 TWITCH: https://www.twitch.tv/daveyx3\n🌍 DISCORD: https://discord.gg/t5WKfF7\n📷 INSTAGRAM: https://www.instagram.com/daveyx3/\n👔 MERCH STORE: https://daveyx3-shop.fourthwall.com/\n👉 2ND CHANNEL: https://bit.ly/daveyx3_offmeta\n\n💻 MY PC GEAR: https://www.amazon.com/shop/daveyx3 (Affiliate)\nIf you purchase anything from this link, I'll receive a small commission!\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\nSeason 14 Volibear Runes: \nExplained at beginning of the video.\n\nSeason 14 Volibear Build:\n(nashor tooth - mejai - rabadon - riftmaker - zhonya)\n▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬▬\n[Concepts of this video + Information + Everything you can find on my Channel]:\nLeague of Legends Gameplay Guides, League Gameplay Guides, LoL Gameplay Guides, League of Legends Guide, League Guide, LoL Guide, Season 14 Guide League of Legends, S14 Guide League of Legends, League of Legends Best Runes Season 14, League of Legends Best Build season 14, League of Legends Best Playstyle Season 14, Season 14 Tips, S14 Tips, League of Legends Tips, League of Legends, Daveyx3 Gameplay Channel.\nI'm a Challenger Top Main. Been challenger since S7/S8/S9/S10 & Season 11 and peaked 1109 LP at most. and I upload new video's everyday!"
+                   },
+                   'defaultAudioLanguage': 'en'
+      },
+      'statistics': {'viewCount': '6584', 
+                     'likeCount': '267', 
+                     'favoriteCount': '0',
+                     'commentCount': '37'}
+    }]
+}
+"""
+
+
+class YoutubeVideoInformation():
+    def __init__(self, data: dict):
+        self._data = data['items'][0]
+        self.title = self._data['snippet']['title']
+        self.description = self._data['snippet']['description']
+        self.views = self._data['statistics']['viewCount']
+        self.likes = self._data['statistics']['likeCount']
+        self.published_at = self._data['snippet']['publishedAt']
+        self.channel = self._data['snippet']['channelTitle']
+
+
+def get_yt_video_information(video_id: str) -> YoutubeVideoInformation:
+    request = get_yt_connection().videos().list(
+        part="snippet,statistics",
+        id=video_id
+    )
+    return YoutubeVideoInformation(request.execute())
+
+
+def main():
+    yt_video_information = get_yt_video_information("l_6I6LChDNk")
+    print(yt_video_information.title)
+
+
+if __name__ == "__main__":
+    main()
